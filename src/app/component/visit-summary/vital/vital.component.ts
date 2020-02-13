b1e5c9fd72447d15ba692188b958ecc6c4a91f2b
kundanmail55 <kundanmail55@gmail.com>
2020-02-13 13:59:33 +0530

    rel 2.2.1

--- src/app/component/visit-summary/vital/vital.component.ts
+++ src/app/component/visit-summary/vital/vital.component.ts
@@ -18,45 +18,48 @@ vitalsPresent = false;
 
   ngOnInit() {
     const visitUuid = this.route.snapshot.paramMap.get('visit_id');
-      this.visitService.fetchVisitDetails(visitUuid)
-      .subscribe(visits => {
-        visits.encounters.forEach(visit => {
-          const display = visit.display;
-          if (visit.display.match('Vitals') !== null ) {
-            this.vitalsPresent = true;
-            this.answer.date = display.split(' ')[1];
-            const vitalUUID = visit.uuid;
-            this.service.vitals(vitalUUID)
-            .subscribe(vitals => {
-              const vital = vitals.obs;
-              vital.forEach(obs => {
-                const displayObs = obs.display;
-                if (displayObs.match('SYSTOLIC') !== null ) {
-                  this.answer.sbp = Number(obs.display.slice(25, obs.display.length));
-                }
-                if (displayObs.match('DIASTOLIC') !== null ) {
-                  this.answer.dbp = Number(obs.display.slice(26, obs.display.length));
-                }
-                if (displayObs.match('Weight') !== null ) {
-                  this.answer.weight = Number(obs.display.slice(13, obs.display.length));
-                }
-                if (displayObs.match('Height') !== null ) {
-                  this.answer.height = Number(obs.display.slice(13, obs.display.length));
-                }
-                if (displayObs.match('BLOOD OXYGEN SATURATION') !== null ) {
-                  this.answer.sp02 = Number(obs.display.slice(25, obs.display.length));
-                }
-                if (displayObs.match('TEMP') !== null ) {
-                  this.answer.temp = Number(obs.display.slice(17, obs.display.length));
-                }
-                if (displayObs.match('Pulse') !== null ) {
-                  this.answer.pulse = Number(obs.display.slice(7, obs.display.length));
-                }
-              });
-              this.v.push(this.answer);
+    this.visitService.fetchVisitDetails(visitUuid)
+    .subscribe(visits => {
+      visits.encounters.forEach(visit => {
+        const display = visit.display;
+        if (visit.display.match('Vitals') !== null ) {
+          this.vitalsPresent = true;
+          this.answer.date = display.split(' ')[1];
+          const vitalUUID = visit.uuid;
+          this.service.vitals(vitalUUID)
+          .subscribe(vitals => {
+            const vital = vitals.obs;
+            vital.forEach(obs => {
+              const displayObs = obs.display;
+              if (displayObs.match('SYSTOLIC') !== null ) {
+                this.answer.sbp = Number(obs.display.slice(25, obs.display.length));
+              }
+              if (displayObs.match('DIASTOLIC') !== null ) {
+                this.answer.dbp = Number(obs.display.slice(26, obs.display.length));
+              }
+              if (displayObs.match('Weight') !== null ) {
+                this.answer.weight = Number(obs.display.slice(13, obs.display.length));
+              }
+              if (displayObs.match('Height') !== null ) {
+                this.answer.height = Number(obs.display.slice(13, obs.display.length));
+              }
+              if (displayObs.match('BLOOD OXYGEN SATURATION') !== null ) {
+                this.answer.sp02 = Number(obs.display.slice(25, obs.display.length));
+              }
+              if (displayObs.match('TEMP') !== null ) {
+                this.answer.temp = Number(obs.display.slice(17, obs.display.length));
+              }
+              if (displayObs.match('Pulse') !== null ) {
+                this.answer.pulse = Number(obs.display.slice(7, obs.display.length));
+              }
+              if (displayObs.match('Respiratory rate') !== null ) {
+                this.answer.respiratoryRate = Number(obs.display.slice(18, obs.display.length));
+              }
             });
-          }
-        });
+            this.v.push(this.answer);
+          });
+        }
       });
-}
+    });
+  }
 }
